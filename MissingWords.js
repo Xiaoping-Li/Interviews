93dbfb10b2d2b9b6208761121dbce6b547b79c63
Xiaoping-Li <narci2008@gmail.com>
2018-10-20 13:38:24 -0700

    Update MissingWords.js

--- MissingWords.js
+++ MissingWords.js
@@ -1,3 +1,26 @@
 /*
 s is a sentence with only words and space, t is subsentence of s. Find all the missing words in t in order
 */
+
+function missingwords (s,t) {
+  const sArray = s.split(' ');
+  const tArray = t.split(' ');
+  const rtn = [];
+  let i = 0;
+  let j = 0;
+  while (i < sArray.length && j < tArray.length) {
+    if (sArray[i] !== tArray[j]) {
+      rtn.push(sArray[i]);
+      i++;
+    } else {
+      i++;
+      j++;
+    }
+  }
+  
+  return rtn.concat(sArray.slice(i));
+}
+
+const s = 'I am using a to improve code';
+const t = 'am a to improve';
+missingwords(s,t);
